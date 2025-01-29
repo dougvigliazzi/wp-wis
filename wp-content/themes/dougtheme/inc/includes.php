7eeb6dc8008bc5c65577763e6411d7ddd4d12eb3
douglas <dougvigliazzi@gmail.com>
2025-01-29 20:21:51 +0100

    themes updated

---
+++ wp-content/themes/dougtheme/inc/includes.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Include custom features etc.
+ *
+ * @package air-light
+ */
+
+namespace Air_Light;
+
+// Theme setup
+require get_theme_file_path( '/inc/includes/theme-setup.php' );
+
+// Localized strings
+require get_theme_file_path( '/inc/includes/localization.php' );
+
+// Nav Walker
+require get_theme_file_path( '/inc/includes/nav-walker.php' );
+
+// Post type and taxonomy base classes
+// We check this with if, because this stuff will not go to WP theme directory
+if ( file_exists( get_theme_file_path( '/inc/includes/taxonomy.php' ) ) ) {
+  require get_theme_file_path( '/inc/includes/taxonomy.php' );
+}
+
+if ( file_exists( get_theme_file_path( '/inc/includes/post-type.php' ) ) ) {
+  require get_theme_file_path( '/inc/includes/post-type.php' );
+}
+
+// Custom functions
